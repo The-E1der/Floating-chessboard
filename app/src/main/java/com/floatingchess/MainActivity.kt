8205c7c602197cad42c36f2530e1aaf3517a3a7b
The-E1der <uvieejiro@gmail.com>
2026-08-08 11:54:13 +0100

    Update MainActivity.kt

--- app/src/main/java/com/floatingchess/MainActivity.kt
+++ app/src/main/java/com/floatingchess/MainActivity.kt
@@ -2,47 +2,23 @@ package com.floatingchess
 
 import android.content.Intent
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
-import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Check if we have overlay permission
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+        if (!Settings.canDrawOverlays(this)) {
             val intent = Intent(
                 Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
                 Uri.parse("package:$packageName")
             )
-            startActivityForResult(intent, 1234)
-            Toast.makeText(this, "Please grant overlay permission to use Floating Chess", Toast.LENGTH_LONG).show()
+            startActivity(intent)
         } else {
-            startFloatingService()
+            startService(Intent(this, FloatingChessService::class.java))
         }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 1234) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Settings.canDrawOverlays(this)) {
-                startFloatingService()
-            }
-        }
-    }
-
-    private fun startFloatingService() {
-        val intent = Intent(this, FloatingChessService::class.java)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            startForegroundService(intent)
-        } else {
-            startService(intent)
-        }
-        finish() // Close main UI, overlay stays active!
+        finish()
     }
 }
